36edd4bc6b989ab3173b73dc03f33e7d4002397f
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-06-04 06:38:23 -0700

    fixing

--- server.js
+++ server.js
@@ -56,8 +56,8 @@ app.use(session(
 //Controllers
 // const userController = require('./controllers/user_controller.js')
 // app.use('/users', userController)
-const logController = require('./controllers/log_controller.js')
-app.use('/logs', logController)
+// const logController = require('./controllers/log_controller.js')
+// app.use('/logs', logController)
 // const sessionController = require('./controllers/session_controller.js')
 // app.use('/sessions', sessionController)
 
