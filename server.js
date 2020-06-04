f6ee92118bb1888a356901f414d1282caf075c3b
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-06-04 06:40:25 -0700

    fixing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
-const session = require('express-session')
+// const session = require('express-session')
 require('dotenv').config()
 //___________________
 //Port
@@ -39,19 +39,19 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 app.use(express.static('public'));
 
 // populates req.body with parsed info from forms - if no data from forms will return an empty object {}
-app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.urlencoded({ extended: true }));// extended: false - does not allow nested objects in query strings
 app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
 
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 // use session
-app.use(session(
-  {
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: false
-  }
-))
+// app.use(session(
+//   {
+//     secret: process.env.SECRET,
+//     resave: false,
+//     saveUninitialized: false
+//   }
+// ))
 
 //Controllers
 // const userController = require('./controllers/user_controller.js')
