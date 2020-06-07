5c33fe6fda777d66246fe0dfba7e6e80ccff95fd
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-06-07 16:05:09 -0700

    floating button working

--- public/js/misc.js
+++ public/js/misc.js
@@ -3,23 +3,9 @@
 
 $(() => {
 
-  let icons = []
-
   $('.sidenav').sidenav();
-
-
-let glycemia = $('.level').text()
-let iconColor = $('.circle')
-
-let color = ''
-
-if(glycemia > 220){
-  color = 'red'
-} else if(glycemia < 220 && glycemia > 110){
-  color = 'orange'
-} else if(glycemia < 110 && glycemia > 90){
-  color = 'green'
+  $('.fixed-action-btn').floatingActionButton();
+  document.getElementById("icon-delete").onclick = function() {
+    document.getElementById("delete").submit();
 }
-
-
 })
