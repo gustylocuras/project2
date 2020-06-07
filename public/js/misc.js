0fd15b0f4a79035880203364362a4f2916734aae
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-06-06 23:08:02 -0700

    starting show page styling

--- public/js/misc.js
+++ public/js/misc.js
@@ -1,26 +1,25 @@
 
+
+
 $(() => {
 
   let icons = []
 
   $('.sidenav').sidenav();
-let glycemia = $('#level').text()
+
+
+let glycemia = $('.level').text()
 let iconColor = $('.circle')
-// if(glycemia > 220){
-//   iconColor.addClass('red')
-// } else if(glycemia < 220 && glycemia > 90){
-//   iconColor.addClass('green')
-// } else if(glycemia < 90){
-//   iconColor.addClass('red')
-// }
-let over220 = Number >220
-let under220 = Number <220
-switch(glycemia) {
-  case over220:
-    iconColor.addClass('red')
-    break;
-  case under220:
-    iconColor.addClass('green')
-    break;
-  }
+
+let color = ''
+
+if(glycemia > 220){
+  color = 'red'
+} else if(glycemia < 220 && glycemia > 110){
+  color = 'orange'
+} else if(glycemia < 110 && glycemia > 90){
+  color = 'green'
+}
+
+
 })
